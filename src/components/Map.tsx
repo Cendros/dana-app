ed83517bca179641ae81c59835ef7eedd1ef37aa
Cendros <ludorjt12@gmail.com>
2023-12-12 16:42:08 +0100

    map interactive

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { mapMarker } from '../consts/map';
 import { LatLngExpression } from 'leaflet';
@@ -9,22 +9,42 @@ type MapProps = {
     zoom: number
     markers?: Array<Marker>
     className?: string
+    onClick?: () => void
 }
 
 export type Marker = {
     coords: LatLngExpression
     popupText?: string
+    onClick?: () => void
 }
 
-const Map: React.FC<MapProps> = ({ center, zoom, markers, className }) => {
+const Map: React.FC<MapProps> = ({ center, zoom, markers, className, onClick }) => {
+    const OnClick = () => {
+        useMapEvents({
+            click() {
+                if (onClick)
+                    onClick();
+            }
+        });
+        return <></>;
+    }
+
     return (
         <MapContainer center={center} zoom={zoom} scrollWheelZoom className={className ?? ''}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            <OnClick />
             { markers ? markers.map((marker, i) => (
-                <Marker key={i} position={marker.coords} icon={mapMarker}>
+                <Marker
+                    key={i}
+                    position={marker.coords}
+                    icon={mapMarker}
+                    eventHandlers={{
+                        click: () => marker.onClick ? marker.onClick() : null
+                    }}
+                >
                     { marker.popupText ?
                         <Popup>{marker.popupText}</Popup>
                     : null }
--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,16 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import { IonContent, IonPage } from '@ionic/react';
+import { IonButton, IonContent, IonImg, IonModal, IonPage, useIonModal } from '@ionic/react';
 import Header from '../components/Header';
 import MapView, { Marker } from '../components/Map';
 import 'leaflet/dist/leaflet.css';
 import Loading from '../components/Loading';
 import useStructures from '../hooks/useStructures';
+import { StructureType } from '../types/structure';
+import useStructure from '../hooks/useStructure';
+import Details from '../components/Check/Structure/Details';
 
 const Map: React.FC = () => {
     const [show, setShow] = useState<boolean>(false);
+    const [selectedStructure, setSelectedStructure] = useState<StructureType | undefined>(undefined);
     const [markers, setMarkers] =  useState<Array<Marker> | undefined>(undefined);
 
     const structures = useStructures();
+
+    const { setStructure } = useStructure();
+
+    const DetailsStructureModal = ({ onDismiss }: { onDismiss: () => void }) => (
+        <Details dismiss={onDismiss} />
+    )
+
+    const [presentModal, dismissModal] = useIonModal(DetailsStructureModal, {
+        onDismiss: () => dismissModal()
+    })
+
+    const structureInfos = (structureId: number) => {
+        setStructure(structureId);
+        presentModal();
+    }
 
     useEffect(() => {
         if (structures == undefined)
@@ -19,6 +38,7 @@ const Map: React.FC = () => {
         const _markers = structures.map(structure => {
             return {
                 coords: [Number(structure.longitude), Number(structure.latitude)],
+                onClick: () => setSelectedStructure(structure)
             } as Marker
         })
 
@@ -31,9 +51,27 @@ const Map: React.FC = () => {
             <Header />
             <IonContent fullscreen className='bg-light'>
                 { show ?
-                    <MapView center={[49.23077763216166, -0.3315435963674239]} zoom={11} className='w-full h-full' markers={markers} />
+                    <MapView center={[49.23077763216166, -0.3315435963674239]} zoom={11} className='w-full h-full' markers={markers} onClick={() => setSelectedStructure(undefined)} />
                 : <Loading text='Chargement de la carte' /> }
             </IonContent>
+            <IonModal isOpen={!!selectedStructure} onDidDismiss={() => setSelectedStructure(undefined)} backdropDismiss={false} initialBreakpoint={.33} backdropBreakpoint={.33} breakpoints={[0, .25]}>
+                { selectedStructure ?
+                    <div className='p-3'>
+                        <h3>{selectedStructure.name}</h3>
+                        <div className='flex flex-row gap-2'>
+                            <div className='aspect-16-9 flex-1 border-round-lg overflow-hidden'>
+                                <IonImg src={selectedStructure.image} className='w-full h-full img-cover' />
+                            </div>
+                            <div className='flex flex-column gap-1 flex-1'>
+                                <span>{selectedStructure.address}</span>
+                                <span>{selectedStructure.city}</span>
+                                <span>{selectedStructure.postalCode}</span>
+                            </div>
+                        </div>
+                        <IonButton className='flex mt-3 text-initial' fill='outline' onClick={() => {structureInfos(selectedStructure.id)}}>Voir plus</IonButton>
+                    </div>
+                : null }
+            </IonModal>
         </IonPage>
     );
 };
